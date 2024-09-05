e088113978900b2ab8bca3b6e0a83445244e1bbd
Zain Ahmed <zainahmed1713@gmail.com>
2024-09-05 22:15:14 +0500

    Create data model

---
+++ models/comment.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    postsID: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: Post
+    },
+    userID: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: User
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true })
+
+export const Comment = mongoose.model("Comment", commentSchema);
---
+++ models/flashcard.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const flashcardSchema = new mongoose.Schema({
+    deckID: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: Deck
+    },
+    question: {
+        type: String,
+        required: true
+    },
+    answer: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+export const Flashcard = mongoose.model("Flashcard", flashcardSchema)
