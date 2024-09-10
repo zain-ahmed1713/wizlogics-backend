b42622c082827b45d25cf602d257dfc61ed41a33
Zain Ahmed <zainahmed1713@gmail.com>
2024-09-10 20:54:15 +0500

    Add custom class to streamline API errors

---
+++ src/utils/APIError.js
@@ -0,0 +1,10 @@
+class APIError extends Error {
+    constructor(status, code, errorMessage) { // E.g., Status=error, code-404, message="Data not found" 
+        super(status, code, errorMessage);
+        this.status = status;
+        this.code = code;
+        this.errorMessage = errorMessage;
+    }
+}
+
+export { APIError }
