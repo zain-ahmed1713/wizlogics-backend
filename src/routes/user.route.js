27fd375850445fac5025b7f667b70ba8453836c6
Zain Ahmed <zainahmed1713@gmail.com>
2024-09-13 02:22:15 +0500

    Implement user login

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { signupUser } from "../controllers/signup.controller.js";
 import { verifyUser } from "../controllers/verifyUser.controller.js";
 import { regenerateOTP } from "../controllers/regenerateOTP.controller.js";
+import { loginUser } from "../controllers/login.controller.js";
 
 const router = Router();
 
 router.route("/register").post(signupUser)
 router.route("/verify").post(verifyUser)
 router.route("/regenerate-otp").post(regenerateOTP)
+router.route("/auth/login").post(loginUser)
 
 export default router;
