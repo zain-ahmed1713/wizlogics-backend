3055ad145567ba368fa68948c011eede14c3cb4a
Zain Ahmed <zainahmed1713@gmail.com>
2024-09-13 22:05:58 +0500

    Implement user logout

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,6 +3,8 @@ import { signupUser } from "../controllers/signup.controller.js";
 import { verifyUser } from "../controllers/verifyUser.controller.js";
 import { regenerateOTP } from "../controllers/regenerateOTP.controller.js";
 import { loginUser } from "../controllers/login.controller.js";
+import { verifyJWT } from "../middleware/verifyJWT.middleware.js";
+import { logoutUser } from "../controllers/logout.controller.js";
 
 const router = Router();
 
@@ -10,5 +12,6 @@ router.route("/register").post(signupUser)
 router.route("/verify").post(verifyUser)
 router.route("/regenerate-otp").post(regenerateOTP)
 router.route("/auth/login").post(loginUser)
+router.route("/auth/logout").post(verifyJWT, logoutUser)
 
 export default router;
