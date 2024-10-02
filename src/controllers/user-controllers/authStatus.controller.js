e3e07e90b1585f35e889a5e864a04192aed51beb
Zain Ahmed <zainahmed1713@gmail.com>
2024-10-02 17:24:29 +0500

    Move auth status controller into user-controller directory

--- src/controllers/user-controllers/authStatus.controller.js
+++ src/controllers/user-controllers/authStatus.controller.js
@@ -1,5 +1,5 @@
-import { APIResponse } from "../utils/APIResponse.js";
-import asyncHandler from "../utils/asyncHandler.js";
+import { APIResponse } from "../../utils/APIResponse.js";
+import asyncHandler from "../../utils/asyncHandler.js";
 
 const authStatus = asyncHandler(async (req, res) => {
     res.json(new APIResponse("success", 200, req.user, "User authenticated successfully"))
