a9c19744aed90d5abaa1ff6797386267871f858d
Zain Ahmed <zainahmed1713@gmail.com>
2024-11-19 00:01:31 +0500

    Fix: Bug fixes

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -4,16 +4,16 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (userEmail, emailSubject, emailBody) => {
     try {
-        const { data, error } = await resend.emails.send({
+        await resend.emails.send({
             from: "WizLogics <onboarding@resend.dev>",
             to: userEmail,
             subject: emailSubject,
             html: emailBody,
         });
 
-        if (error) {
-            throw new Error("Email sending failed. Error:", error)
-        }
+        // if (error) {
+        //     throw new Error("Email sending failed. Error:", error)
+        // }
 
     } catch (error) {
         throw new Error("Resend Email Service Error. Error:", error)
